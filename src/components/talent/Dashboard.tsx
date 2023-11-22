53937d7d6a8493575b09fe047a7b52c195b8e8a0
izeekii <ijames@noemdek.co.uk>
2023-11-22 13:59:33 +0100

    fixes ui in talent dashboard

--- src/components/talent/Dashboard.tsx
+++ src/components/talent/Dashboard.tsx
@@ -10,8 +10,8 @@ const TalentDashboard = () => {
     <MainLayout>
       {" "}
       <Tabs defaultValue="home" className="w-full">
-        <div className="flex flex-col md:flex-row  justify-between w-full pr-4   md:pr-12 xl:pr-40 bg-white  shadow-xl drop-shadow-lg space-y-4 md:space-y-0">
-          <TabsList className=" pl-4  md:pl-12 xl:pl-40">
+        <div className="flex flex-col md:flex-row  justify-between w-full px-4   md:px-12 xl:px-40 bg-white  shadow-xl drop-shadow-lg space-y-4 md:space-y-0">
+          <TabsList className="">
             <TabsTrigger value="home">Home</TabsTrigger>
             <TabsTrigger value="projects">Projects</TabsTrigger>
             <TabsTrigger value="reports">Reports</TabsTrigger>
@@ -29,8 +29,7 @@ const TalentDashboard = () => {
             </div>
           </div>
         </div>
-        <div className="flex overflow-hidden overflow-y-hidden h-[82vh] mb-2">
-          <Footer />
+        <div className="flex overflow-hidden overflow-y-hidden h-[82vh] mb-2 xl:pl-40 md:pl-12 pl-4">
           <TabsContent value="home" className="w-full">
             <Hometab />
           </TabsContent>
