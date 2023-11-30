670420bace5f9f95e75dcb2fdd7990b4de934e54
FalaseNoemdek <ffalase@noemdek.co.uk>
2023-11-30 21:25:34 +0100

    socials

--- src/components/talent/components/Social.tsx
+++ src/components/talent/components/Social.tsx
@@ -141,16 +141,20 @@ export default function Social({
               <div className="relative  z-0 w-full mb-6 group">
                 <input
                   type="text"
-                  name="floating_first_name"
-                  id="floating_first_name"
+                  name="linkedin"
+                  id="linkedin"
                   className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                   placeholder=" "
                   // value={formData.projectDuration.startDate}
                   // onChange={handleInputChange}
+                  value={socials.linkedin}
+                  onChange={(e) =>
+                    handleInputChange("linkedin", e.target.value)
+                  }
                   required
                 />
                 <label
-                  htmlFor="floating_first_name"
+                  htmlFor="linkedin"
                   className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                 >
                   LinkedIn profile
@@ -161,16 +165,20 @@ export default function Social({
               <div className="relative  z-0 w-full mb-6 group">
                 <input
                   type="text"
-                  name="floating_first_name"
-                  id="floating_first_name"
+                  name="instagram"
+                  id="instagram"
                   className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                   placeholder=" "
                   // value={formData.projectDuration.startDate}
                   // onChange={handleInputChange}
+                  value={socials.instagram}
+                  onChange={(e) =>
+                    handleInputChange("instagram", e.target.value)
+                  }
                   required
                 />
                 <label
-                  htmlFor="floating_first_name"
+                  htmlFor="instagram"
                   className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                 >
                   Instagram profile
@@ -181,12 +189,14 @@ export default function Social({
               <div className="relative  z-0 w-full mb-6 group">
                 <input
                   type="text"
-                  name="floating_first_name"
-                  id="floating_first_name"
+                  name="twitter"
+                  id="twitter"
                   className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                   placeholder=" "
                   // value={formData.projectDuration.startDate}
                   // onChange={handleInputChange}
+                  value={socials.twitter}
+                  onChange={(e) => handleInputChange("twitter", e.target.value)}
                   required
                 />
                 <label
@@ -201,12 +211,15 @@ export default function Social({
               <div className="relative  z-0 w-full mb-6 group">
                 <input
                   type="text"
-                  name="floating_first_name"
-                  id="floating_first_name"
+                  name="facebook"
+                  id="facebook"
                   className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                   placeholder=" "
                   // value={formData.projectDuration.startDate}
                   // onChange={handleInputChange}
+                  onChange={(e) =>
+                    handleInputChange("facebook", e.target.value)
+                  }
                   required
                 />
                 <label
@@ -234,6 +247,7 @@ export default function Social({
                 <Button
                   className="dark__btn w-fit whitespace-nowrap"
                   // onClick={next}
+                  // onClick={handleSocial}
                 >
                   Save
                 </Button>
