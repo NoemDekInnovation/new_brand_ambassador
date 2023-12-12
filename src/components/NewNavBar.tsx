b2b7c7b44dab165efed8c9ee884d8c076769f82d
izeekii <ijames@noemdek.co.uk>
2023-12-12 21:01:40 +0100

    creates nav pop up

--- src/components/NewNavBar.tsx
+++ src/components/NewNavBar.tsx
@@ -28,12 +28,17 @@ import { Button } from "../ui/button";
 import { Separator } from "../ui/seperator";
 import { DialogClose } from "@radix-ui/react-dialog";
 import { campaignAuthAxiosInstance } from "../api/axios";
+import NavPreview from "./talent/components/navPreview";
 
 export default function NewNavBar() {
   const user = useSelector((state: RootState) => state.user);
   const [toggleMenubar, setToggleMenubar] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [notifications, setNotifications] = useState<any | null>(null);
+  const [popUp, setPopUp] = useState(false);
+  const [selectedProject, setSelectedProject] = useState();
+  const [projects, setProjects] = useState();
+  const [apply, setApply] = useState(false);
 
   const navigate = useNavigate();
 
@@ -73,229 +78,240 @@ export default function NewNavBar() {
     setIsLoading(false);
   }, [user?.user?.accountId]);
 
-  console.log("notifications,notifications", notifications[0]);
-
   return (
-    <div>
-      <nav className="flex justify-between px-4  md:px-12 xl:px-40 items-center  text-[12px] font-medium bg-white py-7">
-        <div className="flex items-center gap-2 md:gap-8 flex-1">
-          <div className="flex justify-between items-center lg:w-fit">
-            <Link to="/dashboard">
-              <img src={logo} style={{}} alt="logo" width={140} height={50} />
-            </Link>
+    <>
+      <div>
+        <nav className="flex justify-between px-4  md:px-12 xl:px-40 items-center  text-[12px] font-medium bg-white py-7">
+          <div className="flex items-center gap-2 md:gap-8 flex-1">
+            <div className="flex justify-between items-center lg:w-fit">
+              <Link to="/dashboard">
+                <img src={logo} style={{}} alt="logo" width={140} height={50} />
+              </Link>
 
-            <div className="block lg:hidden ">
-              <button
-                id="nav"
-                className="flex items-center px-3 py-2 border-2 rounded text-bm_black border-bm_black hover:text-bm_black hover:border-bm "
-                onClick={handleToggle}
-              >
-                <svg
-                  className="fill-current h-3 w-3"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
+              <div className="block lg:hidden ">
+                <button
+                  id="nav"
+                  className="flex items-center px-3 py-2 border-2 rounded text-bm_black border-bm_black hover:text-bm_black hover:border-bm "
+                  onClick={handleToggle}
                 >
-                  <title>Menu</title>
-                  <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-                </svg>
-              </button>
+                  <svg
+                    className="fill-current h-3 w-3"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <title>Menu</title>
+                    <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+                  </svg>
+                </button>
+              </div>
+            </div>
+            <div className="hidden lg:flex items-center border rounded-md w-full  px-3">
+              <AiOutlineSearch className="text-[15px] " />
+              <Input
+                className="border-0 focus:border-0 focus:ring-0 focus:outline-none "
+                placeholder="Search"
+              />
             </div>
           </div>
-          <div className="hidden lg:flex items-center border rounded-md w-full  px-3">
-            <AiOutlineSearch className="text-[15px] " />
-            <Input
-              className="border-0 focus:border-0 focus:ring-0 focus:outline-none "
-              placeholder="Search"
-            />
-          </div>
-        </div>
-        <div className="hidden  lg:flex items-center ml-8 space-x-5 whitespace-nowrap relative">
-          <Dialog>
-            <DropdownMenu>
-              <DropdownMenuTrigger>
-                {" "}
-                <p>
-                  Notifications{" "}
-                  <span className="text-red-900 absolute -top-[0.6PX]">
-                    {notifications?.length}
-                  </span>
-                </p>{" "}
-              </DropdownMenuTrigger>
-              <DropdownMenuContent className="bg-white p-3">
-                {notifications !== null &&
-                  notifications?.map((info: any, idx: number) => {
-                    return (
-                      <>
-                        <DropdownMenuItem
-                          className={`hover:bg-black/10  ${
-                            !info?.messages[0]?.isRead && "bg-black/10"
-                          }`}
-                          key={idx}
-                        >
-                          {info?.messages[0]?.message}{" "}
-                        </DropdownMenuItem>
-                        <DropdownMenuSeparator className="bg-bm__beige" />
-                      </>
-                    );
-                  })}
-              </DropdownMenuContent>
-            </DropdownMenu>
-            <DialogContent className="bg-[#F3F3F3] max-w-[360px] max-h-[282px] flex flex-col">
-              <DialogHeader>
-                <DialogTitle className="flex items-center justify-center m-3">
-                  <img src={logoutImg} alt="" />
-                </DialogTitle>
-                <DialogDescription className="text-[#252525] font-medium text-[18px] text-center w-[288px] pt-5">
-                  Are you sure you want to logout?
-                </DialogDescription>
-              </DialogHeader>
-              <Separator />
-              <div className="flex justify-between">
-                <button>Cancel</button>
-                <Link
-                  to="/api/auth/signout"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    dispatch(logout(""));
-                    // signOut();
-                    navigate("/auth/login");
-                  }}
-                  className="inline-block px-6 py-3 rounded-md bg-gray-200 text-gray-700 hover:bg-gray-300 transition-colors"
-                >
-                  Logout
-                </Link>
-              </div>
-            </DialogContent>
-          </Dialog>
-          <Dialog>
-            <DropdownMenu>
-              <DropdownMenuTrigger>
-                {" "}
-                <div className="flex items-center space-x-2 ml-4">
-                  <img
-                    src={user?.user?.profilePic || avatar}
-                    width={40}
-                    height={40}
-                    alt=""
-                  />
-                  <div className="flex flex-col w-[47px] h-[30px]">
-                    <p className="text-[12px] font-normal">
-                      {capitalizeFirstLetter(
-                        user?.user?.firstName || user?.user?.agencyName
-                      )}
-                    </p>
-
-                    <p
-                      className="
-              text-[10px] font-normal
-              "
-                    >
-                      {capitalizeFirstLetter(user?.user?.role)}
-                    </p>
-                  </div>
+          <div className="hidden  lg:flex items-center ml-8 space-x-5 whitespace-nowrap relative">
+            <Dialog>
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  {" "}
+                  <p>
+                    Notifications{" "}
+                    <span className="text-red-900 absolute -top-[0.6PX]">
+                      {notifications?.length}
+                    </span>
+                  </p>{" "}
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="bg-white p-3">
+                  {notifications !== null &&
+                    notifications?.map((info: any, idx: number) => {
+                      return (
+                        <>
+                          <DropdownMenuItem
+                            className={`hover:bg-black/10  ${
+                              !info?.messages[0]?.isRead && "bg-black/10"
+                            }`}
+                            key={idx}
+                            onClick={() => setPopUp(!popUp)}
+                          >
+                            {info?.messages[0]?.message}{" "}
+                          </DropdownMenuItem>
+                          <DropdownMenuSeparator className="bg-bm__beige" />
+                        </>
+                      );
+                    })}
+                </DropdownMenuContent>
+              </DropdownMenu>
+              <DialogContent className="bg-[#F3F3F3] max-w-[360px] max-h-[282px] flex flex-col">
+                <DialogHeader>
+                  <DialogTitle className="flex items-center justify-center m-3">
+                    <img src={logoutImg} alt="" />
+                  </DialogTitle>
+                  <DialogDescription className="text-[#252525] font-medium text-[18px] text-center w-[288px] pt-5">
+                    Are you sure you want to logout?
+                  </DialogDescription>
+                </DialogHeader>
+                <Separator />
+                <div className="flex justify-between">
+                  <button>Cancel</button>
+                  <Link
+                    to="/api/auth/signout"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      dispatch(logout(""));
+                      // signOut();
+                      navigate("/auth/login");
+                    }}
+                    className="inline-block px-6 py-3 rounded-md bg-gray-200 text-gray-700 hover:bg-gray-300 transition-colors"
+                  >
+                    Logout
+                  </Link>
                 </div>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent className="bg-white p-3">
-                <DropdownMenuItem className="hover:bg-black/10">
-                  <Link to="/profile">Profile</Link>
-                </DropdownMenuItem>
-                <DropdownMenuSeparator className="bg-bm__beige" />
-                <DropdownMenuItem className="hover:bg-black/10">
-                  Billing & Payments
-                </DropdownMenuItem>
-                <DropdownMenuSeparator className="bg-bm__beige" />
-                <DropdownMenuItem className="hover:bg-black/10">
-                  Settings
-                </DropdownMenuItem>
-                <DropdownMenuSeparator className="bg-bm__beige" />
-                <DropdownMenuItem className="hover:bg-black/10">
-                  <DialogTrigger>Logout</DialogTrigger>
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-            <DialogContent className="bg-[#F3F3F3] max-w-[360px] max-h-[282px] flex flex-col">
-              <DialogHeader>
-                <DialogTitle className="flex items-center justify-center m-3">
-                  <img src={logoutImg} alt="" />
-                </DialogTitle>
-                <DialogDescription className="text-[#252525] font-medium text-[18px] text-center w-[288px] pt-5">
-                  Are you sure you want to logout?
-                </DialogDescription>
-              </DialogHeader>
-              <Separator />
-              <div className="flex justify-between">
-                <button>Cancel</button>
-                <Link
-                  to="/api/auth/signout"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    dispatch(logout(""));
-                    // signOut();
-                    navigate("/auth/login");
-                  }}
-                  className="inline-block px-6 py-3 rounded-md bg-gray-200 text-gray-700 hover:bg-gray-300 transition-colors"
-                >
-                  Logout
-                </Link>
-              </div>
-            </DialogContent>
-          </Dialog>
-        </div>
-      </nav>
+              </DialogContent>
+            </Dialog>
+            <Dialog>
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  {" "}
+                  <div className="flex items-center space-x-2 ml-4">
+                    <img
+                      src={user?.user?.profilePic || avatar}
+                      width={40}
+                      height={40}
+                      alt=""
+                    />
+                    <div className="flex flex-col w-[47px] h-[30px]">
+                      <p className="text-[12px] font-normal">
+                        {capitalizeFirstLetter(
+                          user?.user?.firstName || user?.user?.agencyName
+                        )}
+                      </p>
 
-      <div
-        className={`flex flex-col lg:hidden  space-y-5 transition-all duration-200 ${
-          // toggleMenubar ? "opacity-100 h-100 " : "opacity-0 h-0"
-
-          toggleMenubar
-            ? "opacity-100 h-100"
-            : "-translate-y-[1000px] opacity-0 h-0"
-        }`}
-      >
-        <div className="flex flex-col lg:hidden  space-y-5 p-3">
-          <div className="flex bg-white items-center border rounded-md w-full px-3">
-            <AiOutlineSearch className="text-[15px] " />
-            <Input
-              className="border-0 focus:border-0 focus:ring-0 focus:outline-none "
-              placeholder="Search"
-            />
-          </div>
-          <p>Messages</p>
-          <div className="flex items-center space-x-2 ml-4">
-            <div className="flex flex-col">
-              <p className="text-[12px] font-normal">{user?.user?.firstName}</p>
-
-              <p
-                className="
+                      <p
+                        className="
               text-[10px] font-normal
               "
-              >
-                {user?.user?.role}
-              </p>
-            </div>
+                      >
+                        {capitalizeFirstLetter(user?.user?.role)}
+                      </p>
+                    </div>
+                  </div>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="bg-white p-3">
+                  <DropdownMenuItem className="hover:bg-black/10">
+                    <Link to="/profile">Profile</Link>
+                  </DropdownMenuItem>
+                  <DropdownMenuSeparator className="bg-bm__beige" />
+                  <DropdownMenuItem className="hover:bg-black/10">
+                    Billing & Payments
+                  </DropdownMenuItem>
+                  <DropdownMenuSeparator className="bg-bm__beige" />
+                  <DropdownMenuItem className="hover:bg-black/10">
+                    Settings
+                  </DropdownMenuItem>
+                  <DropdownMenuSeparator className="bg-bm__beige" />
+                  <DropdownMenuItem className="hover:bg-black/10">
+                    <DialogTrigger>Logout</DialogTrigger>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+              <DialogContent className="bg-[#F3F3F3] max-w-[360px] max-h-[282px] flex flex-col">
+                <DialogHeader>
+                  <DialogTitle className="flex items-center justify-center m-3">
+                    <img src={logoutImg} alt="" />
+                  </DialogTitle>
+                  <DialogDescription className="text-[#252525] font-medium text-[18px] text-center w-[288px] pt-5">
+                    Are you sure you want to logout?
+                  </DialogDescription>
+                </DialogHeader>
+                <Separator />
+                <div className="flex justify-between">
+                  <button>Cancel</button>
+                  <Link
+                    to="/api/auth/signout"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      dispatch(logout(""));
+                      // signOut();
+                      navigate("/auth/login");
+                    }}
+                    className="inline-block px-6 py-3 rounded-md bg-gray-200 text-gray-700 hover:bg-gray-300 transition-colors"
+                  >
+                    Logout
+                  </Link>
+                </div>
+              </DialogContent>
+            </Dialog>
           </div>
+        </nav>
 
-          <Link to="/profile">
-            <div className="hover:bg-black/10">Profile</div>
-          </Link>
-          <DropdownMenuSeparator className="bg-bm__beige" />
-          <div className="hover:bg-black/10">Billing & Payments</div>
-          <DropdownMenuSeparator className="bg-bm__beige" />
-          <div className="hover:bg-black/10">Settings</div>
-          <DropdownMenuSeparator className="bg-bm__beige" />
-          <Link
-            to="/api/auth/signout"
-            onClick={(e) => {
-              e.preventDefault();
-              dispatch(logout(""));
-              // signOut();
-              navigate("/auth/login");
-            }}
-          >
-            Logout
-          </Link>
+        <div
+          className={`flex flex-col lg:hidden  space-y-5 transition-all duration-200 ${
+            // toggleMenubar ? "opacity-100 h-100 " : "opacity-0 h-0"
+
+            toggleMenubar
+              ? "opacity-100 h-100"
+              : "-translate-y-[1000px] opacity-0 h-0"
+          }`}
+        >
+          <div className="flex flex-col lg:hidden  space-y-5 p-3">
+            <div className="flex bg-white items-center border rounded-md w-full px-3">
+              <AiOutlineSearch className="text-[15px] " />
+              <Input
+                className="border-0 focus:border-0 focus:ring-0 focus:outline-none "
+                placeholder="Search"
+              />
+            </div>
+            <p>Messages</p>
+            <div className="flex items-center space-x-2 ml-4">
+              <div className="flex flex-col">
+                <p className="text-[12px] font-normal">
+                  {user?.user?.firstName}
+                </p>
+
+                <p
+                  className="
+              text-[10px] font-normal
+              "
+                >
+                  {user?.user?.role}
+                </p>
+              </div>
+            </div>
+
+            <Link to="/profile">
+              <div className="hover:bg-black/10">Profile</div>
+            </Link>
+            <DropdownMenuSeparator className="bg-bm__beige" />
+            <div className="hover:bg-black/10">Billing & Payments</div>
+            <DropdownMenuSeparator className="bg-bm__beige" />
+            <div className="hover:bg-black/10">Settings</div>
+            <DropdownMenuSeparator className="bg-bm__beige" />
+            <Link
+              to="/api/auth/signout"
+              onClick={(e) => {
+                e.preventDefault();
+                dispatch(logout(""));
+                // signOut();
+                navigate("/auth/login");
+              }}
+            >
+              Logout
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
+      <NavPreview
+        popUp={popUp}
+        close={() => setApply(false)}
+        setApply={() => setApply(true)}
+        setPopUp={() => setPopUp(!popUp)}
+        selectedProject={selectedProject}
+        apply={apply}
+      />
+    </>
   );
 }
---
+++ src/components/talent/components/navPreview.tsx
@@ -0,0 +1,95 @@
+import React, { useRef, useState } from "react";
+import { Card, CardContent } from "../../../ui/card";
+import { ImAttachment, ImCancelCircle } from "react-icons/im";
+import { Separator } from "../../../ui/seperator";
+import ItemCard from "./ItemCard";
+import { Button } from "../../../ui/button";
+import {
+  DayObject,
+  daysOfWeekx,
+} from "../../agency/createproject/projectBudget";
+import { GiPaperClip } from "react-icons/gi";
+import { IoMdShare } from "react-icons/io";
+import { Input } from "../../../ui/input";
+
+const NavPreview = ({
+  popUp,
+  setPopUp,
+  selectedProject,
+  apply,
+  close,
+  setApply,
+}: {
+  setApply: any;
+  close: any;
+  apply: boolean;
+  popUp: boolean;
+  setPopUp: any;
+  selectedProject: any;
+}) => {
+  const startDate = new Date(selectedProject?.projectDuration?.startDate);
+  const endDate = new Date(selectedProject?.projectDuration?.endDate);
+  const formattedStartDate = startDate.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+
+  const formattedEndDate = endDate.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+
+  const [selectedFileName, setSelectedFileName] = useState("");
+  const [document, setDocument] = useState<File>({} as File);
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, files } = e.target;
+    const fileInput = fileInputRef.current;
+    if (fileInput && fileInput.files && fileInput.files.length > 0) {
+      setSelectedFileName(fileInput.files[0].name);
+    }
+
+    if (files?.length) {
+      const selectedFile = Array.from(files);
+      setDocument(selectedFile[0]);
+    }
+  };
+
+  const handleDivClick = () => {
+    // Trigger a click event on the hidden input
+    fileInputRef?.current?.click();
+  };
+
+  return (
+    <div
+      className={`fixed z-[1000] bg-black/50  w-[100%] items-center justify-end flex flex-col transition-all duration-1000 inset-0 ${
+        popUp
+          ? "translate-y-0 opacity-100 h-[100vh] -bottom-5"
+          : "translate-y-[1000px] opacity-0 "
+      }`}
+    >
+      <Card className="p-4 relative bg-white w-[90VW] h-[95vh] overflow-hidden md:p-10">
+        <div className="flex justify-between w-full">
+          Notification Title{" "}
+          <span className=" text-sm text-[#6F797A]">
+            <ImCancelCircle
+              className="w-[20px] h-[20px] cursor-pointer ml-2 md:ml-6"
+              onClick={() => {
+                close();
+                setPopUp();
+              }}
+            />
+          </span>
+        </div>
+        <Separator className="bg-bm__beige my-2 mb-10 " />
+        Notification body
+      </Card>
+    </div>
+  );
+};
+
+export default NavPreview;
