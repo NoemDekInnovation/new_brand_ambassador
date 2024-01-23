cf6d151663bd08e104c3408648b4d002c46eafc7
izeekii <ijames@noemdek.co.uk>
2024-01-23 01:20:07 +0100

    implemenst group shortlisting and rejecteing on application screen

--- src/components/agency/appliedTalents/AllApplications.tsx
+++ src/components/agency/appliedTalents/AllApplications.tsx
@@ -40,7 +40,13 @@ const AllApplications = ({
   appStatus,
   gap,
   ProjectId,
+  handleGroupSubmit,
+  handleCheckedChange,
+  bulkModal,
+  setBulkModal,
 }: {
+  bulkModal?: any;
+  setBulkModal?: any;
   ProjectId: string;
   appStatus: string;
   gridView: boolean;
@@ -57,6 +63,8 @@ const AllApplications = ({
   gap?: number;
   projectModal?: any;
   setProjectModal?: any;
+  handleCheckedChange?: any;
+  handleGroupSubmit?: any;
 }) => {
   const { talents: resTalents } = useSelector(
     (state: RootState) => state.talent
@@ -174,6 +182,7 @@ const AllApplications = ({
                 selectedProject={""}
                 setSuccessModal={""}
                 successModal={true}
+                handleCheckedChange={handleCheckedChange}
                 appStatus={appStatus}
               />
             );
@@ -312,7 +321,69 @@ const AllApplications = ({
         )}
                
       </Dialog>
+      <Dialog open={bulkModal} onOpenChange={setBulkModal}>
+        {bulkModal && (
+          <div className=" p-4 h-screen fixed inset-0 bg-black/20 z-[2000]">
+            <DialogContent className="sm:max-w-[425px] bg-[#fff] z-[2000]">
+              <DialogHeader>
+                <DialogTitle className="text-[18px] flex items-center gap-2 cursor-pointer">
+                  Send Offer
+                </DialogTitle>
+              </DialogHeader>
+              <Separator className="bg-[#D7D8DA]" />
 
+              <div className="w-full pb-2">
+                <Select
+                  onValueChange={(e) => setSelectedTalent(e)}
+                  defaultValue={selectedTalent}
+                >
+                  <SelectTrigger className="w-full bg-white  h-[46px]">
+                    <SelectValue placeholder="Select Offer" />
+                  </SelectTrigger>
+                  <SelectContent className="bg-white z-[3000]">
+                    <SelectItem value="supervisor">Supervisor</SelectItem>
+                    <SelectItem value="usher">Usher</SelectItem>
+                    <SelectItem value="brand ambassador">
+                      Brand Ambassador
+                    </SelectItem>
+                  </SelectContent>
+                </Select>
+              </div>
+
+              <Separator className="bg-[#D7D8DA]" />
+
+              <DialogFooter className="">
+                <Button
+                  type="submit"
+                  className="dark__btn h-[46px]"
+                  // onClick={handleGroupSubmit}
+                >
+                  {isLoading && (
+                    <svg
+                      aria-hidden="true"
+                      role="status"
+                      className="inline w-4 h-4 mr-3 text-white animate-spin"
+                      viewBox="0 0 100 101"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                        fill="#E5E7EB"
+                      />
+                      <path
+                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                        fill="currentColor"
+                      />
+                    </svg>
+                  )}
+                  Send Offer
+                </Button>
+              </DialogFooter>
+            </DialogContent>
+          </div>
+        )}
+      </Dialog>
       <Dialog open={successModal}>
         <DialogContent className="bg-bm_card_grey flex flex-col items-center justify-center max-w-[360px] md:max-w-[500px] max-h-[300px] py-8">
           <TbProgressCheck className="font-normal text-[155px] text-green-700" />
